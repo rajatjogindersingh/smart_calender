de7f9e410d59ec38d6a87798db4c54f099f1127d
Rajat Singh <rajat.singh@indiqube.com>
2020-04-01 20:52:19 +0530

    Changed directory structure to avoid circular dependency

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,8 @@
 from config import SmartCalenderConfig
 from flask import Flask
-from flask_restful import Api
 from mongoengine import *
-from app.api.user_service import UserRegistrationService, UserLoginService
+
 
 app = Flask(__name__)
 app.config.from_object(SmartCalenderConfig)
 db = connect(app.config["MONGO_DB_NAME"])
-api = Api(app)
-
-api.add_resource(UserRegistrationService, '/api/user_service/user_registration')
-api.add_resource(UserLoginService, '/api/user_service/login')
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3.7
+from app import app
+from app.api.user_service import UserRegistrationService, UserLoginService
+from flask_restful import Api
 
 if __name__ == "main":
     try:
-        from app import app
+        api = Api(app)
+        api.add_resource(UserRegistrationService, '/api/user_service/user_registration')
+        api.add_resource(UserLoginService, '/api/user_service/login')
     except Exception as e:
         print(e)
