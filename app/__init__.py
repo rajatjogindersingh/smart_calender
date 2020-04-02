6bf10603f0cf4cb48ff2fe4ced7c82d3ed03039a
Rajat Singh <rajat.singh@indiqube.com>
2020-04-02 18:17:04 +0530

    Added support for JWT token validation

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from config import SmartCalenderConfig
 from flask import Flask
 from mongoengine import *
 import os
+from app.utililty.middleware import validate_jwt
 
 
 app = Flask(__name__)
 app.config.from_object(SmartCalenderConfig)
 app.config['SECRET_KEY'] = os.urandom(24)
 db = connect(app.config["MONGO_DB_NAME"])
+validate_jwt(app)
---
+++ app/utililty/middleware.py
@@ -0,0 +1,42 @@
+from flask import request, g, Response
+import jwt
+import json
+from app.model.user import User
+
+
+exception_from_jwt = ['login', 'register']
+
+
+def validate_jwt(app):
+    @app.before_request
+    def validate():
+        # To give exemption to login and register from jwt token verification
+        if any(i in request.url for i in exception_from_jwt):
+            return
+        else:
+            msg = None
+            auth_token = request.headers.get('Authorization')
+            if auth_token is None:
+                msg = 'Authorization token missing'
+
+            if not msg:
+
+                try:
+                    payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
+                    email = payload['email']
+                    user = User.objects(email=email)
+
+                    # Invalid email id in token
+                    if not user:
+                        msg = 'Token is corrupt'
+                    else:
+                        g.user_info = user[0]
+
+                except jwt.ExpiredSignatureError:
+                    msg = 'Signature expired. Please log in again.'
+                except jwt.InvalidTokenError:
+                    msg = 'Invalid token. Please log in again.'
+
+            if msg:
+                return Response(response=json.dumps({"message": msg}), status=400, content_type="application/json")
+            return
