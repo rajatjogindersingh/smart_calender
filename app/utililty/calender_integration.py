03c6f0d5c1f70592373dda2362a542af0233f080
Rajat Singh <rajat.singh@indiqube.com>
2020-04-06 00:29:20 +0530

    Removed open browser call from google calender api

--- app/utililty/calender_integration.py
+++ app/utililty/calender_integration.py
@@ -28,7 +28,7 @@ def main(event: dict):
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=0, open_browser=False)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
